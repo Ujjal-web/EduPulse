b0b764354e9311d3f8ced9e275cc1549a023b77c
Eistiak Ahmed <eistiakahmedmeraj@gamil.com>
2026-02-06 23:34:34 +0600

    Home Page Design Start

--- Frontend/src/Pages/Home/HomePage.jsx
+++ Frontend/src/Pages/Home/HomePage.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <div>
+    <div className='bg-green-400'>
+      <h1 className='text-4xl font-bold text-center py-5'>Welcome to EduPulse</h1>
       
     </div>
   );
